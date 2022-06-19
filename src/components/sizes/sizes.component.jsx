36d90ae13dbcca37ad3e134f5c54642ab0b5302f
ted <tpprogrammingcontact@gmail.com>
2022-06-19 14:06:07 +0300

    fixing button bugs

--- src/components/sizes/sizes.component.jsx
+++ src/components/sizes/sizes.component.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import './sizes.styles.css';
+import './sizes.styles.scss';
 
 const Sizes = () => {
     const [sizes, setSizes] = useState('');
@@ -15,10 +15,10 @@ const Sizes = () => {
         <>
         {/* <h2>Selected size = {sizes}</h2> */}
         <div className='sizes-container'>
-            <span onClick={() => setSizes('XS')} className={sizes=='XS' ? 'selected' : null}>XS</span>
-            <span onClick={() => setSizes('S')} className={sizes=='S' ? 'selected' : null}>S</span>
-            <span onClick={() => setSizes('M')} className={sizes=='M' ? 'selected' : null}>M</span>
-            <span onClick={() => setSizes('L')} className={sizes=='L' ? 'selected' : null}>L</span>
+            <span onClick={() => setSizes('XS')} className={sizes==='XS' ? 'selected' : null}>XS</span>
+            <span onClick={() => setSizes('S')} className={sizes==='S' ? 'selected' : null}>S</span>
+            <span onClick={() => setSizes('M')} className={sizes==='M' ? 'selected' : null}>M</span>
+            <span onClick={() => setSizes('L')} className={sizes==='L' ? 'selected' : null}>L</span>
         </div>
         </>
     )
